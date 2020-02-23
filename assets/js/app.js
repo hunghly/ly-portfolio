c5b2f6f4c4510aadc0663fc666d64935b43dbaae
Hung Ly <hunghly@gmail.com>
2020-02-23 15:17:22 -0600

    worked on portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+{
+    $(document).ready(() => {
+        const aboutMeEl = $("#about-me-message")[0];
+        console.log(aboutMeEl.innerHTML);
+        const aboutMeMessages = ['am a USAF vet', 'am a software developer', 'also have a passion for learning', 'solve problems'];
+
+        const displayAboutMe = () => {
+            let i = 0;
+            let intervalID = setInterval(() => {
+                $(aboutMeEl).addClass('rollIn').removeClass('rollOut').html(aboutMeMessages[i]);
+                i++;
+                if (i === aboutMeMessages.length) clearInterval(intervalID);
+                else {
+                    setTimeout(() => {
+                        $(aboutMeEl).addClass('rollOut').removeClass('rollIn')
+                    }, 3000);
+                }
+            }, 3500)
+        };
+
+
+
+        displayAboutMe();
+    });
+}
