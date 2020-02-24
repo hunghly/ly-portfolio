252b5c37d4a76f5620988366798a1c742251c4fe
Hung Ly <hunghly@gmail.com>
2020-02-24 12:27:12 -0600

    worked more on project sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 {
     $(document).ready(() => {
         const aboutMeEl = $("#about-me-message")[0];
-        console.log(aboutMeEl.innerHTML);
         const aboutMeMessages = ['am a USAF veteran', 'am a Software Developer', 'love to learn', 'am also passionate about coding', 'solve problems'];
+        const project2HeadingMarkup = `
+                <div class="col-sm-6 col-xs-12">
+                    <p><span>Hangman</span><br>
+                        utilizes HTML canvas, game logic, JS modules to play Hangman.
+                    </p>
+                </div>`;
+
+        const project2VideoMarkup = `
+                <div class="col-sm-6 col-xs-12">
+                    <video controls>
+                    <source src="./assets/demos/HangmanDemo720.mov" type="video/mp4">
+                </video>
+                </div>`;
+        let previousView = (window.outerWidth >= 576) ? 'default' : 'x-small';
+        let currentView = (window.outerWidth >= 576) ? 'default' : 'x-small';
 
         const displayAboutMe = () => {
             let i = 0;
@@ -18,8 +32,38 @@
             }, 3000)
         };
 
+        const xSmallView = () => {
+            $("#project-2").html(`${project2HeadingMarkup}${project2VideoMarkup}`);
+        };
+
+        const defaultView = () => {
+            $("#project-2").html(`${project2VideoMarkup}${project2HeadingMarkup}`);
+        };
+
+        const setEvenProjectHTML = (view) => {
+            if (view === 'default') defaultView();
+            else xSmallView();
+        };
+
+        const setCurrentView = () => {
+            currentView = (window.outerWidth >= 576) ? 'default' : 'x-small';
+        };
+
+        const setEvenProjects = () => {
+            setCurrentView();
+            if (currentView !== previousView) {
+                setEvenProjectHTML(currentView);
+                previousView = currentView;
+            }
+        };
 
 
+        window.addEventListener("resize", function (e) {
+            setEvenProjects();
+        });
+
+        setEvenProjects();
+        setEvenProjectHTML(currentView);
         displayAboutMe();
     });
 }
