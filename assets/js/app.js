9278cb1c76aa13f7880d25245683f8e03ba93a84
Hung Ly <hunghly@gmail.com>
2020-02-23 20:37:37 -0600

    worked on portfolio's experience

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
     $(document).ready(() => {
         const aboutMeEl = $("#about-me-message")[0];
         console.log(aboutMeEl.innerHTML);
-        const aboutMeMessages = ['am a USAF vet', 'am a software developer', 'also have a passion for learning', 'solve problems'];
+        const aboutMeMessages = ['am a USAF vet', 'am a software developer', 'love to learn', 'am also passionate about code', 'solve problems'];
 
         const displayAboutMe = () => {
             let i = 0;
