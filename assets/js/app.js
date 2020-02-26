a25bdfb81b217a6bf1e6e39ed64685d77d7d39d2
Hung Ly <hunghly@gmail.com>
2020-02-25 19:14:23 -0600

    changed image

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@
         const project2HeadingMarkup = `
                 <div class="col-sm-6 col-xs-12">
                     <p><span>Hangman</span><br>
-                        learned HTML canvas, game logic, dynamic HTML generation to create a simple Hangman app.
+                        HTML canvas, game logic, dynamic HTML generation to create a simple Hangman app.
                     </p>
                     <a href="https://github.com/hunghly/sample-portfolio-code/tree/master/hangman" target="_blank">code photos</a>
                 </div>`;
